4dd5289c4ec29b4a68477414584e1656218495a2
Ingvar Derešivski <inde999@gmail.com>
2024-12-12 13:33:06 +0200

    Datatypes

---
+++ 01. 12.12.2024/DataType/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data types!");
+
+            //täisnumber
+            int bigNumber = 124444445;
+            Console.WriteLine(bigNumber);
+
+            //komaga number
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
+
+            //tähemärk
+            string stringVar = "asdasd12354@#   $%&*()-+";
+            Console.WriteLine(stringVar);
+
+            float floatVarible = 123.123F;
+            Console.WriteLine(floatVarible);
+
+            char charVarible = '2';
+            Console.WriteLine(charVarible);
+
+            bool boolVarible = false;
+            Console.WriteLine(boolVarible);
+        }
+    }
+}
