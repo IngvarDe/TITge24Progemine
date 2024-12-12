abf21e50713a71e8dacce37d507575d1e32747f7
Ingvar Derešivski <inde999@gmail.com>
2024-12-12 14:07:42 +0200

    RectangleCalc

---
+++ 01. 12.12.2024/RectangleCalculator/RectangleCalculator/RectangleCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RectangleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ristküliku pindala ja ümbermõõt!");
+
+            Console.WriteLine("Enter first value");
+            string firstNumber = Console.ReadLine();
+            float floatFirst = float.Parse(firstNumber);
+
+            Console.WriteLine("Enter second value");
+            string secondNumber = Console.ReadLine();
+            float floatSecond = float.Parse(secondNumber);
+
+            //teha pindala ja ümbermõõdu kalkulatsioon
+            Console.WriteLine("Ümbermõõt " + 2 * (floatFirst + floatSecond));
+            Console.WriteLine("Pindala " + (floatFirst * floatSecond));
+            Console.ReadKey();
+        }
+    }
+}
