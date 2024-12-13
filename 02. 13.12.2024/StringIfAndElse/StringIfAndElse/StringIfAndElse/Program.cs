27221ec7e6edd0d490c079925df686fc4e4dcbca
Ingvar Derešivski <inde999@gmail.com>
2024-12-13 09:08:39 +0200

    StringIfAndElse

---
+++ 02. 13.12.2024/StringIfAndElse/StringIfAndElse/StringIfAndElse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StringIfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //peab saama sisestada nime
+            //if ja else kontrollib, kas on nimi või on tühi
+            //kui leiate kuskilt teksti värviliseks teha, siis kasutage
+            Console.WriteLine("Sisesta nimi ja vajuta enter");
+            string firstName = Console.ReadLine();
+
+            if (firstName != "")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Tere " + firstName);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+            }
+        }
+    }
+}
