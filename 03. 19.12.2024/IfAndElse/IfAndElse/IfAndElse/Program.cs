e455feb15eefc8041d271eb650c2f2b66942ac8e
Ingvar Derešivski <inde999@gmail.com>
2024-12-19 14:04:58 +0200

    IfElseGitTest

---
+++ 03. 19.12.2024/IfAndElse/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dogs!");
+
+            string dog = Console.ReadLine();
+
+            if (dog == "hunt")
+            {
+                Console.WriteLine("Sul on hundikoer");
+            }
+            else if (dog == "taksi")
+            {
+                Console.WriteLine("Sul on taksikoer");
+            }
+            else
+            {
+                Console.WriteLine("Sul on krants");
+            }
+            //kood tuleb githubi panna ja kasutada git programmi
+            //õpetused asuvad Tahvlis
+            //ise valid ühe git programmi välja
+        }
+    }
+}
