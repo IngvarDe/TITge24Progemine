a4cf41f6c7b1d95a2e24e24f5d503e0bb72aa5bd
Ingvar Derešivski <inde999@gmail.com>
2024-12-19 14:54:44 +0200

    RingCalculation

---
+++ 03. 19.12.2024/RingCalculation/RingCalculation/RingCalculation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RingCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Ruudu sees asub ring. Ringi raadius on 3.
+            //Leia ja väljasta ekraanile ruudu pindala ja ruudu ümbermõõt.
+            //Leia ja väljasta ekraanile ringi pindala ja ringi ümbermõõt.
+
+            int roundRadius = 3;
+            int squareSide = 6;
+
+            Console.WriteLine("Ruudu pindala " + squareSide * squareSide);
+            Console.WriteLine("Ruudu ümbermõõt " + 4 * squareSide);
+
+            Console.WriteLine("Ringi pindala " + Math.PI * (roundRadius * roundRadius));
+            Console.WriteLine("Ringi ümbermõõt " + 2 * Math.PI * roundRadius);
+
+
+        }
+    }
+}
