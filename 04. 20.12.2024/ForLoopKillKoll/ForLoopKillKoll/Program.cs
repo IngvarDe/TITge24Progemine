f79a188636d859845153b8cf8e691e13269c7885
Ingvar Derešivski <inde999@gmail.com>
2024-12-20 10:17:18 +0200

    ForLoopNesting

---
+++ 04. 20.12.2024/ForLoopKillKoll/ForLoopKillKoll/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ForLoopKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta korduste arv");
+            /*Ülesanne 3
+            Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            kill-koll kill-koll killadi-koll kill-koll kill-koll 
+            killadi-koll kill-koll kill-koll killadi-koll*/
+            //for loop for loopi sees
+
+            int arv = Int16.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= arv; i++)
+            {
+                Console.WriteLine("See rida on nr {0}", i);
+                string a = "kill-koll";
+                string b = "killadi-koll";
+
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.WriteLine(a);
+                }
+
+                for (int k = 1; k <= 1; k++)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+        }
+    }
+}
