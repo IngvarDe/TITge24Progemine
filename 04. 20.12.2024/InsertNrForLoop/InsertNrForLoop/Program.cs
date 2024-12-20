9ecc604ffe648bcb669d53b5a5a2777ad79f2943
Ingvar Derešivski <inde999@gmail.com>
2024-12-20 09:37:47 +0200

    ForLoopInNumber

---
+++ 04. 20.12.2024/InsertNrForLoop/InsertNrForLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace InsertNrForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta tärnide arv");
+            Console.WriteLine("Kasutan selle jaoks for loopi");
+
+            int number = Int16.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
