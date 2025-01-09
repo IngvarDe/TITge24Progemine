9c4d2f375683ebb05f821b5cd983a12da97e075b
Ingvar Derešivski <inde999@gmail.com>
2025-01-09 14:51:02 +0200

    MethodCall

---
+++ 05. 09.01.2025/MethodCall/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Jalgratas()
+        {
+            Console.WriteLine("N[[d oled jalgratta meetodis");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main meetodi algus");
+
+            //kutsume esile Jalgratta meetodi
+            Jalgratas();
+
+            Console.WriteLine("See on main meetodi lõpp");
+        }
+
+        //teha uus meetod nimega Jalgratas
+    }
+}
