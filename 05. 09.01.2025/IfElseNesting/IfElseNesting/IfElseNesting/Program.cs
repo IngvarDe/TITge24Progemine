e2d02ddbdf009fee05000785e5f31a94a2e45c70
Ingvar Derešivski <inde999@gmail.com>
2025-01-09 14:12:59 +0200

    IfElseNesting

---
+++ 05. 09.01.2025/IfElseNesting/IfElseNesting/IfElseNesting/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IfElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If ja else nestimine");
+            Console.WriteLine("Sisesta kujundi nimetus:");
+            Console.WriteLine("nurgelised või ringjad");
+
+            // teha enda valikul [ks if ja else nestimine
+            //teema m]
+            string shape = Console.ReadLine();
+
+            if (shape == "nurgelised")
+            {
+                Console.WriteLine("Sisesta nelinurk või nurgeline");
+                string corner = Console.ReadLine();
+                if(corner == "nelinurk")
+                {
+                    Console.WriteLine("See on nelinurk");
+                }
+                else
+                {
+                    Console.WriteLine("See on äkki mingi muu nurgeline");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Siin on mingid ringjad kujundid");
+            }
+        }
+    }
+}
