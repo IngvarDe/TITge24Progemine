ae8a398071950bb94fc5a621df8950f4c2ff6e03
Ingvar Derešivski <inde999@gmail.com>
2025-01-09 13:46:23 +0200

    IfAndElse

---
+++ 05. 09.01.2025/IfAndElse/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ainult number");
+
+            string age = Console.ReadLine();
+            int ageConverted = int.Parse(age);
+
+            if (ageConverted >= 0 && ageConverted <= 18)
+            {
+                Console.WriteLine("Oled alaealine");
+            }
+            else if (ageConverted >= 18 && ageConverted < 65)
+            {
+                Console.WriteLine("Oled täisealine");
+            }
+            else
+            {
+                Console.WriteLine("Oled pensionäär");
+            } 
+            //if ja else rakendus tuleb teha
+            //kontrollib sinu vanust
+            //1. kui oled alla 18, siis konsool annab vastuseks,
+            //et oled alaealine
+
+            //2. Kui oled 19 kuni 65 aastat vana, siis
+            // konsool vastab, et oled täisealine
+
+            //3. Kui oled üle 65 a vana, siis pensionäär
+        }
+    }
+}
