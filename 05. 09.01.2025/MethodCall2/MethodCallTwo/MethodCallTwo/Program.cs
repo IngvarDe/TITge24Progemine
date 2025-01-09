175668e936d722398e6aa7af7c10384f12325f83
Ingvar Derešivski <inde999@gmail.com>
2025-01-09 15:03:45 +0200

    MethodCall2

---
+++ 05. 09.01.2025/MethodCall2/MethodCallTwo/MethodCallTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MethodCallTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //siin kasutada if ja else
+            //m]lemas valikus peab v'lja kutsuma meetodi
+            //teemaks paaris ja paaritud arvud
+            string nr = Console.ReadLine();
+            int number = Convert.ToInt32(nr);
+
+            if (int.IsEvenInteger(number))
+            {
+                EvenNumbers(number);
+            }
+            else
+            {
+                OddNumbers(number);
+            }
+        }
+
+        static void OddNumbers(int nr)
+        {
+            Console.WriteLine("Tegemist on paaritu arvuga " + nr);
+        }
+        static void EvenNumbers(int nr)
+        {
+            Console.WriteLine("Tegemist on paaris arvuga " + nr);
+
+        }
+    }
+}
