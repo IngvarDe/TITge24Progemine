1eec0c13f27cb931592d39a7b7d0ad96611952f5
Ingvar Derešivski <inde999@gmail.com>
2025-01-10 09:46:37 +0200

    SwitchLetters

---
+++ 06. 10.01.2025/SwitchLetters/SwitchLetters/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SwitchLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta vokaal (a, e, i) ja vajuta enter");
+
+            string ch = Console.ReadLine();
+
+            switch (ch.ToLower())
+                //kui kasutad ToLower, siis ei ole vaja teist case
+            {
+                case "a":
+                //case "A":
+                    Console.WriteLine("Sisestasid vokaali: " + ch);
+                    break;
+
+                case "e":
+                //case "E":
+                    Console.WriteLine("Sisestasid vokaali: " + ch);
+                    break;
+
+                case "i":
+                //case "I":
+                    Console.WriteLine("Sisestasid vokaali: " + ch);
+                    break;
+
+                //default:
+                    Console.WriteLine("Ei ole vokaal");
+                    break;
+            }
+        }
+    }
+}
