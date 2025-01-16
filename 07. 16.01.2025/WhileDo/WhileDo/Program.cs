5b0f5e949448fffa4a9e5f4babe019169b0135e6
Ingvar Derešivski <inde999@gmail.com>
2025-01-16 15:01:53 +0200

    WhileDo

---
+++ 07. 16.01.2025/WhileDo/WhileDo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WhileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While and Do");
+
+            int number = 1;
+
+            do
+            {
+                //peab numbreid n'itama 1-st kuni 5-ni
+                Console.WriteLine(number);
+                number++;
+            }
+            while (number <= 5);
+        }
+    }
+}
