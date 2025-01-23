08ac67dd82b610733fbacd17b26a8089290627e9
Ingvar Derešivski <inde999@gmail.com>
2025-01-23 13:31:39 +0200

    ForTereTulemast

---
+++ 08. 23.01.2025/HelloForLoop/HelloForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("---------------");
+
+            //teha konsoolirakendus ja kasutada for loopi
+            //kolm korda n'idatakse Tere tulemast
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Tere tulemast");
+            }
+        }
+    }
+}
