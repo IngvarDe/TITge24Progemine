196c5c7a310d0b145e4dad97e69dd68ed2206258
Ingvar Derešivski <inde999@gmail.com>
2025-01-23 14:47:00 +0200

    ForLoopTriangle

---
+++ 08. 23.01.2025/TriangleForLoop/TriangleForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TriangleForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmnurk tärnidest e poolik püramiid");
+            Console.Write("----------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Sisesta ridade arv: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
