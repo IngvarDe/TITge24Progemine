2a3663526431b9eec4e6ff769b710fe75cbc8297
Ingvar Derešivski <inde999@gmail.com>
2025-01-23 13:13:13 +0200

    ForLoopSquare

---
+++ 08. 23.01.2025/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ruudu tegemine");
+
+            Console.WriteLine("Sisesta ruudu suurus");
+            int squareSize = Int32.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= squareSize; row++)
+            {
+                for (int column = 1; column <= squareSize; column++)
+                {
+                    string variable = (row == column || row + column == squareSize + 1) ? " X " : " 0 ";
+
+                    Console.Write(variable);
+                }
+
+                Console.WriteLine();
+        }
+
+            Console.ReadKey();
+        }
+    }
+}
