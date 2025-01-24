30edd22c6dfae09f06b332495a9f8b7cb15cbed1
Ingvar Derešivski <inde999@gmail.com>
2025-01-24 09:02:46 +0200

    EndlessLoop

---
+++ 09. 24.01.2025/ForUshort/ForUshort/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForUshort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // hakkab itereerima nii kaua kuni ushort 
+            // andmetüübist saadakse kätte maksimaalne arv e 63 tuhat
+            // peale seda alustab uuesti
+            // lõputu itereerimine tuleneb tingimuste puudumisest
+            // kasutada for loopi
+            ushort i = 0;
+            for (;;)
+            {
+                Console.WriteLine("i väärtus on: {0}", i);
+                i++;
+            }
+        }
+    }
+}
