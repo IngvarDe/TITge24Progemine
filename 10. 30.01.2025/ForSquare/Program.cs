aa0acb9e87c5f9ef57d5352f25c4591f3909da48
Ingvar Derešivski <inde999@gmail.com>
2025-01-30 17:31:24 +0200

    SquareAndnameLength

---
+++ 10. 30.01.2025/ForSquare/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ForSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kas soovid ruutu näha?");
+            Console.WriteLine("ei või ja");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "ja")
+            {
+                Square();
+            }
+            else
+            {
+                Console.WriteLine("Mingi error");
+            }
+            //teha ruut ja kasutada for loopi
+            //ruudu valmimine toimub teises meetodis
+            //main meetodis saab k[sida, et kas soovid v]i mitte ruutu teha
+
+
+        }
+
+        static void Square()
+        {
+            Console.WriteLine("Sisesta ruudu suurus");
+            int size = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= size; row++)
+            {
+                for (int column = 1; column <= size * 2; column++)
+                {
+                    string mark;
+                    if(row == column || row + column <= size + 1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                //Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 10. 30.01.2025/ForSquare/StringLengthCompare/StringLengthCompare/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace StringLengthCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] animalName = { "Koer", "Kass", "Jänes", "Sipelgasiil", "Lendorav" };
+            /*
+            >>>>>>>>>>
+            Koer, kass, jänes, sipelgasiil ja lendorav hakkasid vaidlema, 
+            kellel on kõige pikem nimetus. Koosta programm, kus on antud loomanimede järjend.
+            Loo funktsioon, mis saab argumendina ette looma nime ning tagastab (mitte ei trüki!) 
+            selle pikkuse. Lase programmil kutsuda funktsiooni välja
+            iga loomanimede järjendi elemendiga ning trükib ekraanile nime pikkuse. 
+            Pärast pikkuste kontrollimist peab programm väljastama kõige pikema loomanime
+            (võrdsete pikkuste puhul võib esitada vaid ühe pikimatest).'
+            */
+            int longerName = animalName[0].Length;
+            string longerNameString = animalName[0];
+
+            for (int i = 0; i < animalName.Length; i++)
+            {
+                if (longerName < animalName[i].Length)
+                {
+                    longerName = animalName[i].Length;
+                    longerNameString = animalName[i];
+                }
+
+                int animalLength = AnimalNameLength(animalName[i]);
+                Console.WriteLine(animalName[i] + " = ");
+                Console.WriteLine(animalLength);
+            }
+
+            Console.WriteLine("Pikim nimi oli {0} ja mille nime pikkus on: {1}", longerNameString, longerName);
+        }
+
+        static int AnimalNameLength(string animalName)
+        {
+            return animalName.Length;
+        }
+    }
+}
