9929b661aa274e5a75c483c5599ae6f1a470b69d
Ingvar Derešivski <inde999@gmail.com>
2025-01-30 15:28:09 +0200

    ForTriangleNumbers3

---
+++ 10. 30.01.2025/ForTriangleNumbers2/ForTriangleNumbers2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForTriangleNumbers2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta ridadae arv!");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}", i);
+                }
+                Console.WriteLine();
+            }
+            //vooskeem teha
+        }
+    }
+}
