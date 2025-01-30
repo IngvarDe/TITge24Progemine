025d5032fe232b99adf5f7ca22553a5cdab121ad
Ingvar Derešivski <inde999@gmail.com>
2025-01-30 14:45:26 +0200

    ForNumberTriangle

---
+++ 10. 30.01.2025/ForTriangleNumber/ForTriangleNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForTriangleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbrite kolmnurk!");
+
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta ridade arv");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}  ", j);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
