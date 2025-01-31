f24e0c2a499985fae810692117ae367dad3d67b8
Ingvar Derešivski <inde999@gmail.com>
2025-01-31 11:15:46 +0200

    ForeachSorting

---
+++ 11. 31.01.2025/ForeachSorting/ForeachSorting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ForeachSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] genders = { "m", "f", "m", "m", "m", "f", "f", "m", "m", "f" };
+            int female = 0, male = 0;
+
+            foreach (var gender in genders)
+            {
+                //if (gender == "f")
+                //{
+                //    female++;
+                //}
+                //if (gender == "m")
+                //{
+                //    male++;
+                //}
+
+                var result = gender == "f" ? female++ : male++;
+            }
+
+            Console.WriteLine("Number of female: {0}", female);
+            Console.WriteLine("Number of male: {0}", male);
+        }
+    }
+}
