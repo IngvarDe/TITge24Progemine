9fc33e9f616e5f1998ae49fd228447b7e21179a9
Ingvar Derešivski <inde999@gmail.com>
2025-02-07 11:04:16 +0200

    FibonacciFor

---
+++ 11. 31.01.2025/NumberLoop/NumberLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NumberLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foreach and for loop!");
+
+            int[] oddArray = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+
+            foreach (int i in oddArray)
+            {
+                Console.WriteLine(i);
+
+                if (i == 13)
+                {
+                    break;
+                }
+                //kuni 13 numbrini ja siis lõpetab loopimise ära
+            }
+
+            for (int i = 1; i < (99 + 1); i++)
+            {
+                if (i <= 14)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
