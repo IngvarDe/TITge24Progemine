b35532267687b88532064fe5822689a909501b35
Ingvar Derešivski <inde999@gmail.com>
2025-02-13 17:21:11 +0200

    ListForeach

---
+++ 13. 13.02.2025/ListForeach/ListForeach/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Autoosade list");
+
+            //teha vooskeem
+
+            var parts = new List<CarPart>()
+            {
+                new() {Id = 1, PartName = "crank arm"},
+                new() {Id = 2, PartName = "chain ring"},
+                new() {Id = 3, PartName = "regular seat"},
+                new() {Id = 4, PartName = "banana seat"},
+                new() {Id = 5, PartName = "cassette"},
+                new() {Id = 6, PartName = "shift lever"}
+            };
+
+            foreach (var part in parts)
+            {
+                Console.WriteLine(part.Id + " " + part.PartName);
+            }
+        }
+    }
+
+    public class CarPart
+    {
+        public int Id { get; set; }
+        public string PartName { get; set; }
+    }
+}
