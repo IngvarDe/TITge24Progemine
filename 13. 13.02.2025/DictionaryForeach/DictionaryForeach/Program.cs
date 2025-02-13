37bec102bdd690474a1c3b1afb934e535d78d4fc
Ingvar Derešivski <inde999@gmail.com>
2025-02-13 18:22:26 +0200

    ForeachNesting

---
+++ 13. 13.02.2025/DictionaryForeach/DictionaryForeach/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DictionaryForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+
+            var domains = new Dictionary<string, string>()
+            {
+                { "est", "Estonia" },
+                { "lat", "Latvia" },
+                { "fin", "Finland" },
+                { "swe", "Sweden" }
+            };
+
+            int i = 0;
+            foreach (var pair in domains)
+            {
+                i++;
+                Console.WriteLine($"{pair.Key} - {pair.Value} - {i}");
+            }
+
+            //peate kasutama foreachi
+        }
+    }
+}
