a7d813c118c3573246706625f7f772d58aab1035
Ingvar Derešivski <inde999@gmail.com>
2025-02-14 11:40:29 +0200

    RandomAsterisk

---
+++ 14. 14.02.2025/RandomAsterisk/RandomAsterisk/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RandomAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+           /*
+           Ülesanne:
+           >>>>>>>>>>
+           Loo funktsioon, mis saab argumendina
+           ette täisarvu ja trükib ekraanile sama suure
+           arvu tärne. Koosta programm, mis
+           kutsub seda funktsiooni välja 30 korda
+           juhuslike positiivsete täisarvudega,
+           mis on väiksemad kui 20.
+           Näiteks:
+           ************
+           *********
+           **********************
+           *******
+           */
+           int stringRowMin = 0;
+           int stringRowMax = 10;
+
+            int[] randomAsterisk = new int[stringRowMax];
+
+            Random asterisk = new Random();
+            for (int i = stringRowMin; i < randomAsterisk.Length; i++)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                    asterisk.Next(1,20))));
+            }
+        }
+    }
+}
