9f93cb805122726f11bda2f0232eb9366f47ade0
Ingvar Derešivski <inde999@gmail.com>
2025-02-14 11:15:02 +0200

    SortArray

---
+++ 14. 14.02.2025/ArraySortNames/ArraySortNames/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArraySortNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array sort names");
+
+            string[] animals = {"cat", "alligator", "fox",
+                "donkey", "bear", "elephant", "goat" };
+
+            //tuleb kasutada sort-i ja mingit loopi
+            //Array.Sort(animals);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("----------------");
+            Array.Sort(animals, 0, 3);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("-----------------");
+            int[] numbers = { 1, 2, 3, 4, 3, 55, 23, 2 };
+            //tuleb välistada kordused
+            int[] distinct = numbers.Distinct().ToArray();
+            foreach (int number in distinct)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
