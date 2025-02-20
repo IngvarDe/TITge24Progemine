28d4fc431b64019e03af41be44c065e299f34f86
Ingvar Derešivski <inde999@gmail.com>
2025-02-20 17:32:50 +0200

    enum

---
+++ 15. 20.02.2025/EnumProject/EnumProject/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EnumProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum!");
+
+            //kutsu välja enumist esmaspäev
+            Console.WriteLine(Weekdays.Monday);
+
+            int day = (int)Weekdays.Friday;
+            Console.WriteLine(day);
+
+            var weekDay = (Weekdays)1;
+            Console.WriteLine(weekDay);
+
+            //lubab ainult neid väärtuseid sisestada,
+            //mis on enum classis defineeritud
+        }
+
+        enum Weekdays
+        {
+            Monday,
+            Tuesday, 
+            Wednesday, 
+            Thursday,
+            Friday, 
+            Saturday, 
+            Sunday
+        }
+    }
+}
