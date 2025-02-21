025f642e6ad8214ccaf729061b538eb433864c73
Ingvar Derešivski <inde999@gmail.com>
2025-02-21 11:17:19 +0200

    StructProject

--- 16. 21.02.2025/StructProject/StructProject/Program.cs
+++ 16. 21.02.2025/StructProject/StructProject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StructProject
+﻿using System.ComponentModel;
+
+namespace StructProject
 {
     internal class Program
     {
@@ -12,6 +14,15 @@
 
             Console.WriteLine("------------------");
             //kuidas kutsuda IntAndString v'lja???
+            Console.WriteLine("Sisesta nimi ja vajuta enter:");
+            string someName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta vanus ja vajuta enter:");
+            int someAge = Int32.Parse(Console.ReadLine());
+
+            IntAndString intAndString = new(intAndString.Name = someName, intAndString.Age = someAge);
+            Console.WriteLine(intAndString.Name);
+            Console.WriteLine(intAndString.Age);
         }
     }
 
@@ -36,8 +47,8 @@
         public IntAndString(string name, int age)
         {
 
-            Name = name;
-            Age = age;
+            this.Name = name;
+            this.Age = age;
         }
     }
 }
