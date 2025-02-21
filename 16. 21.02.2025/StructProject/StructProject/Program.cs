3993ad811088b9e1d7e05956b5d17941c734aec6
Ingvar Derešivski <inde999@gmail.com>
2025-02-21 10:44:45 +0200

    StructInProgress

---
+++ 16. 21.02.2025/StructProject/StructProject/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StructProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct");
+
+            Coordinate point = new Coordinate();
+            Console.WriteLine(point.X);
+            Console.WriteLine(point.Y);
+
+            Console.WriteLine("------------------");
+            //kuidas kutsuda IntAndString v'lja???
+        }
+    }
+
+    struct Coordinate
+    {
+        public int X;
+        public int Y;
+
+        //kontruktor teha
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    struct IntAndString
+    {
+        public string Name;
+        public int Age;
+
+        public IntAndString(string name, int age)
+        {
+
+            Name = name;
+            Age = age;
+        }
+    }
+}
