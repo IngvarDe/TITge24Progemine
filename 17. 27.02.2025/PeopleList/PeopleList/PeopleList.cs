736a3375e04a53b9c0c08a95fc088636a509bc6d
Ingvar Derešivski <inde999@gmail.com>
2025-02-27 16:49:09 +0200

    LINQThenbySelect

---
+++ 17. 27.02.2025/PeopleList/PeopleList/PeopleList.cs
@@ -0,0 +1,51 @@
+﻿namespace PeopleList
+{
+    public class PeopleList
+    {
+        public static readonly List<People> peoples = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Joonas",
+                Age = 33,
+                GenderId = Guid.Parse("9df54680-4f76-4c26-a55a-92cf012fa71b")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Moona",
+                Age = 18,
+                GenderId = Guid.Parse("fe7f4110-d1b9-4ebc-8219-b48c852be3e6")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 21,
+                GenderId = Guid.Parse("9df54680-4f76-4c26-a55a-92cf012fa71b")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Mary",
+                Age = 20,
+                GenderId = Guid.Parse("fe7f4110-d1b9-4ebc-8219-b48c852be3e6")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 17,
+                GenderId = Guid.Parse("fe7f4110-d1b9-4ebc-8219-b48c852be3e6")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 19,
+                GenderId = Guid.Parse("fe7f4110-d1b9-4ebc-8219-b48c852be3e6")
+            },
+        };
+    }
+}
---
+++ 17. 27.02.2025/PeopleList/PeopleList/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace PeopleList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+
+            Console.WriteLine("Tee valik numbrina");
+            Console.WriteLine("1. ThenBy LINQ");
+            Console.WriteLine("2. ThenByDescending LINQ");
+            Console.WriteLine("3. Select LINQ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    ThenByLINQ();
+                    break;
+
+                case 2:
+                    ThenByDescendingLINQ();
+                    break;
+
+                case 3:
+                    SelectLINQ();
+                    break;
+
+                default:
+                    Console.WriteLine("Vale valik");
+                    break;
+            }
+        }
+
+        public static void ThenByLINQ()
+        {
+            //mida teeb thenby
+            //sorteerib t'hestiku j'rjekorras
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            Console.WriteLine("ThenBy järgi");
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            //mida teeb thenby
+            //sorteerib t'hestiku j'rjekorras
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            Console.WriteLine("ThenByDescending järgi");
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void SelectLINQ()
+        {
+            var selectResult = PeopleList.peoples
+                .Select(x => new
+                {
+                    Name = x.Name,
+                    Age = x.Age
+                });
+
+            Console.WriteLine("Select järgi");
+            foreach (var people in selectResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+    }
+}
