7ae8d77811e3da82d2cc7de728ee2d74f04d909b
Ingvar Derešivski <inde999@gmail.com>
2025-03-13 17:32:41 +0200

    Stopwatch2

---
+++ 20. 13.03.2025/StopWatch2/StopWatch2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace StopWatch2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stopwatch!");
+            //siin algab stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //toimub for loop 1000 korda
+            //seal sees Sleep(10)
+            //prindib välja iga tsükli numbri
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+                Console.WriteLine(i);
+            }
+
+            //stopwatch lõppeb ära
+            stopwatch.Stop();
+
+            //konsoolis näitab aega
+            Console.WriteLine("Kulunud aeg: {0:hh\\:mm\\:ss}",
+                stopwatch.Elapsed);
+        }
+    }
+}
